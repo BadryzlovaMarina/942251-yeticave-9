6af2202fa34a2f725ede5ac909f7d23b9aa38632
Marina <enchanted_night@mail.ru>
2019-04-23 02:14:01 +0700

    task8

--- templates/index.php
+++ templates/index.php
@@ -29,8 +29,8 @@
                         <span class="lot__amount">Стартовая цена</span>
                         <span class="lot__cost"><?=esc(format_cost($item['price'])); ?></span>
                     </div>
-                    <div class="lot__timer timer">
-                        12:23
+                    <div class="lot__timer timer <?php if ($time_count<='01:00'): ?>timer--finishing<?php endif ?>">
+                        <?=$time_count; ?>
                     </div>
                 </div>
             </div>
