856b669a16a4e837ab101b85b2351d16828b8b88
Marina <enchanted_night@mail.ru>
2019-06-03 02:17:58 +0700

    task14

---
+++ sign-up.php
@@ -0,0 +1,68 @@
+<?php
+require_once('functions.php');
+require_once('init.php');
+
+$sql_category = "SELECT id, name, symbol_code FROM category";
+$category = get_mysql_result($link, $sql_category);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $required = ['name', 'email', 'password', 'contact'];
+    $errors = [];
+    
+    foreach ($required as $key) {
+        if (empty($_POST[$key])) {
+            $errors[$key] = 'Поле не заполнено';
+        }
+        elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            $errors['email'] = 'Некорректный email';
+        }
+    }
+    
+    $email = mysqli_real_escape_string($link, $_POST['email']);
+    $sql = "SELECT id FROM user WHERE email = '$email'";
+    $res = mysqli_query($link, $sql);
+
+    if (mysqli_num_rows($res) > 0) {
+        $errors['email'] = 'Пользователь с этим email уже зарегистрирован';
+    }
+    
+    if (count($errors)) {
+        $page_content = include_template('sign-up.php', [
+            'category' => $category,
+            'errors' => $errors,
+            'post' => $_POST
+        ]);
+    }
+        
+    else {
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+        $data = [$_POST['name'], $_POST['email'], $password, $_POST['contact']];
+        $sql = 'INSERT INTO user (name, email, password, contact) VALUES (?,?,?,?)';
+        $stmt = db_get_prepare_stmt($link, $sql, $data);
+        $res = mysqli_stmt_execute($stmt);
+        if ($res) {
+            $lot_id = mysqli_insert_id($link);
+            header('Location: /');
+        }
+        else {
+            http_response_code(404);
+            $page_content = include_template('404.php', []);
+        }
+    }
+}
+else{
+	$page_content = include_template('sign-up.php', [
+        'category' => $category
+    ]);
+}
+
+$layout_content = include_template('layout.php', [
+    'content' => $page_content,
+    'title' => 'Регистрация',
+    'category' => $category,
+    'user_name' => $user_name,
+    'is_auth' => $is_auth
+]);
+
+print($layout_content);
---
+++ templates/sign-up.php
@@ -0,0 +1,52 @@
+<?php
+    $name = isset($post['name']) ? $post['name'] : "";
+    $email = isset($post['email']) ? $post['email'] : "";
+    $password = isset($post['password']) ? $post['password'] : "";
+    $contact = isset($post['contact']) ? $post['contact'] : "";
+?>
+<?php $classname_form = isset($errors) ? "form--invalid" : ""; ?>
+<form class="form container <?=$classname_form; ?>" action="sign-up.php" method="post" autocomplete="off"> <!-- form--invalid -->
+   
+    <h2>Регистрация нового аккаунта</h2>
+    <?php $classname = isset($errors['email']) ? "form__item--invalid" : ""; ?>
+    <div class="form__item <?=$classname; ?>"> <!-- form__item--invalid -->
+        <label for="email">E-mail <sup>*</sup></label>
+        <input id="email" type="text" value="<?= $email; ?>" name="email" placeholder="Введите e-mail">
+        <?php if (isset($errors['email'])): ?>
+            <span class="form__error"><?= $errors['email']; ?></span>
+        <?php endif; ?>
+    </div>
+    
+    <?php $classname = isset($errors['password']) ? "form__item--invalid" : ""; ?>
+    <div class="form__item <?=$classname; ?>">
+        <label for="password">Пароль <sup>*</sup></label>
+        <input id="password" type="password" value="<?= $password; ?>" name="password" placeholder="Введите пароль">
+        <?php if (isset($errors['password'])): ?>
+            <span class="form__error"><?= $errors['password']; ?></span>
+        <?php endif; ?>
+    </div>
+    
+    <?php $classname = isset($errors['name']) ? "form__item--invalid" : ""; ?>
+    <div class="form__item <?=$classname; ?>">
+        <label for="name">Имя <sup>*</sup></label>
+        <input id="name" type="text" value="<?= $name; ?>" name="name" placeholder="Введите имя">
+        <?php if (isset($errors['name'])): ?>
+            <span class="form__error"><?= $errors['name']; ?></span>
+        <?php endif; ?>
+    </div>
+    
+    <?php $classname = isset($errors['contact']) ? "form__item--invalid" : ""; ?>
+    <div class="form__item <?=$classname; ?>">
+        <label for="message">Контактные данные <sup>*</sup></label>
+        <textarea id="message" value="<?= $contact; ?>" name="contact" placeholder="Напишите как с вами связаться"></textarea>
+        <?php if (isset($errors['contact'])): ?>
+            <span class="form__error"><?= $errors['contact']; ?></span>
+        <?php endif; ?>
+    </div>
+    
+    <?php if (isset($errors)): ?>
+        <span class="form__error form__error--bottom">Пожалуйста, исправьте ошибки в форме.</span>
+    <?php endif; ?>
+    <button type="submit" class="button">Зарегистрироваться</button>
+    <a class="text-link" href="#">Уже есть аккаунт</a>
+  </form>
